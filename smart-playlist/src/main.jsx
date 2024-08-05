43b8eb212d8370d5bc52e7cb8fc119a2a045b718
Andrew Whitman <andrew24.whitman@gmail.com>
2024-08-04 23:39:09 -0500

    whitespace formatting

--- smart-playlist/src/main.jsx
+++ smart-playlist/src/main.jsx
@@ -6,36 +6,36 @@ import { ThemeProvider } from '@emotion/react'
 import { createTheme } from '@mui/material'
 
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#20B2AA', // LightSeaGreen
-    },
-    secondary: {
-      main: '#FF6F61', // Soft Coral
-    },
-    background: {
-      default: '#F5F5F5', // Cool White
-    },
-    text: {
-      primary: '#333333', // Charcoal Gray
-    },
-  },
-  typography: {
-    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
-    h1: {
-      fontWeight: 700,
-    },
-    h2: {
-      fontWeight: 600,
-    },
-    // Customize other typography styles here
-  },
+	palette: {
+		primary: {
+			main: '#20B2AA', // LightSeaGreen
+		},
+		secondary: {
+			main: '#FF6F61', // Soft Coral
+		},
+		background: {
+			default: '#F5F5F5', // Cool White
+		},
+		text: {
+			primary: '#333333', // Charcoal Gray
+		},
+	},
+	typography: {
+		fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+		h1: {
+			fontWeight: 700,
+		},
+		h2: {
+			fontWeight: 600,
+		},
+		// Customize other typography styles here
+	},
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
-  // </React.StrictMode>,
+	// <React.StrictMode>
+	<ThemeProvider theme={theme}>
+		<App />
+	</ThemeProvider>
+	// </React.StrictMode>,
 )
